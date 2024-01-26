1e60851c94efc332a26719f2033fcd1a2d056fe9
Scott Carter <carterscottm@users.noreply.github.com>
2024-01-26 13:20:18 -0500

    Update Update-ADK25398BootImageWithCU.ps1
    - Updated WINPECU to latest available.
    - Added documentation on where to get the latest CU
    - Additional code comments

--- Scripts/Update-ADK25398BootImageWithCU.ps1
+++ Scripts/Update-ADK25398BootImageWithCU.ps1
@@ -7,14 +7,20 @@
 # $DISMFile = 'dism.exe'
 
 # Configuring the script to use the Windows ADK 10 version of DISM
-$DISMFile = 'C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\DISM\dism.exe'
-$WinPECU = "E:\Setup\Windows Update For Windows ADK SEP 2023 - Build 25398\windows11.0-kb5032202-x64_4adcdf7b5de0c8498b4739583eb36f89b3865494.msu"
+$DISMFile = "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\DISM\dism.exe"
+
+# LCU can be obtained from "https://catalog.update.microsoft.com/Search.aspx?q=Microsoft%20Server%20Operating%20System-23H2", plug the full file path for the CU below.
+$WinPECU= "E:\Setup\Windows Update For Windows ADK SEP 2023 - Build 25398\windows11.0-kb5034130-x64_b3cf34a9418baf27d88e2760c940e49875be9849.msu"
+
+# Path DISM will use to mount the boot image for servicing
 $BootImageMountFolder = "C:\Mount_BootImage"
+
+# Path and name of the boot image
 $BootImage = "E:\MDTProduction\Boot\LiteTouchPE_x64.wim"
 
 # Verify that files and folder exist
 If (!(Test-Path $DISMFile)){ Write-Warning "DISM in Windows ADK not found, aborting..."; Break }
-if (!(Test-Path -path $WinPECU)) {Write-Warning "Could not find WiNpe Update. Aborting...";Break}
+if (!(Test-Path -path)) {Write-Warning "Could not find WinPE Update. Aborting...";Break}
 if (!(Test-Path -path $BootImage)) {Write-Warning "Could not find Boot image. Aborting...";Break}
 
 # Create Mount folder if it does not exist
